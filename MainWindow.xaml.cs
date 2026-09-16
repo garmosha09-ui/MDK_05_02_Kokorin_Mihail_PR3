2156ee969b49272069a66bfccd864046313f3885
Kokorin Mihail <garmosha_09@mail.ru>
2026-09-16 15:33:04 +0500

    Программирование окна MainWindow. Фамилия

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Security.Cryptography.X509Certificates;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows;
@@ -20,14 +21,69 @@ namespace Chess_Kokorin
     /// </summary>
     public partial class MainWindow : Window
     {
+        public static MainWindow mainWindow;
+        public List<Classes.Pawn> Pawns = new List<Classes.Pawn>();
         public MainWindow()
         {
             InitializeComponent();
+            MainWindow.mainWindow = this;
+            Pawns.Add(new Classes.Pawn(0, 1, false));
+            Pawns.Add(new Classes.Pawn(1, 1, false));
+            Pawns.Add(new Classes.Pawn(2, 1, false));
+            Pawns.Add(new Classes.Pawn(3, 1, false));
+            Pawns.Add(new Classes.Pawn(4, 1, false));
+            Pawns.Add(new Classes.Pawn(5, 1, false));
+            Pawns.Add(new Classes.Pawn(6, 1, false));
+            Pawns.Add(new Classes.Pawn(7, 1, false));
+
+            Pawns.Add(new Classes.Pawn(0, 6, true));
+            Pawns.Add(new Classes.Pawn(1, 6, true));
+            Pawns.Add(new Classes.Pawn(2, 6, true));
+            Pawns.Add(new Classes.Pawn(3, 6, true));
+            Pawns.Add(new Classes.Pawn(4, 6, true));
+            Pawns.Add(new Classes.Pawn(5, 6, true));
+            Pawns.Add(new Classes.Pawn(6, 6, true));
+            Pawns.Add(new Classes.Pawn(7, 6, true));
+
+            CreateFigure();
+        }
+        public void CreateFigure()
+        {
+            foreach (Classes.Pawn Pawn in Pawns)
+            {
+                Pawn.Figure = new Grid()
+                {
+                    Width = 50,
+                    Height = 50
+                };
+                if(Pawn.Black)
+                    Pawn.Figure.Background = new ImageBrush(new BitmapImage(new Uri(@"pack://application:,,,/Images/Pawn (black).png")));
+                else
+                    Pawn.Figure.Background = new ImageBrush(new BitmapImage(new Uri(@"pack://application:,,,/Images/Pawn.png")));
+                Grid.SetColumn(Pawn.Figure, Pawn.X);
+                Grid.SetRow(Pawn.Figure, Pawn.Y);
+                Pawn.Figure.MouseDown += Pawn.SelectFigure;
+                gameBoard.Children.Add(Pawn.Figure);
+            }
         }
 
+        public void OnSelect(Classes.Pawn SelectPawn)
+        {
+            foreach (Classes.Pawn Pawn in Pawns)
+                if (Pawn != SelectPawn)
+                    if (Pawn.Select)
+                        Pawn.SelectFigure(null, null);
+        }
         private void SelectTile(object sender, MouseButtonEventArgs e)
         {
-            
+            Grid Tile = sender as Grid;
+            int X = Grid.GetColumn(Tile);
+            int Y = Grid.GetRow(Tile);
+            Classes.Pawn SelectPawn = Pawns.Find(x => x.Select == true);
+            if (SelectPawn != null)
+            {
+                SelectPawn.Transform(X, Y);
+            }
         }
     }
 }
